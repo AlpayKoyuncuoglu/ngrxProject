3ce11fd4c85975255807b423cf2b581e44723654
akoyuncuoglu <akoyuncuoglu@PC-KOYUNCUOGLU>
2022-02-15 17:07:08 +0300

    6th commit

--- ngrxEx/src/app/counter/counter-output/counter-output.component.ts
+++ ngrxEx/src/app/counter/counter-output/counter-output.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { getCounter } from '../state/counter.selectors';
 import { CounterState } from '../state/counter.state';
 
 @Component({
@@ -11,19 +12,26 @@ import { CounterState } from '../state/counter.state';
 export class CounterOutputComponent implements OnInit {
 
   counter: number;
-  counter$: Observable<{ counter: number }>;
+  //counter$: Observable<{ counter: number }>;
+  counter$: Observable<number>
 
   // constructor(private store: Store<{ counter: { counter: number } }>) { }
   constructor(private store: Store<{ counter: CounterState }>) { }
 
   ngOnInit(): void {
     //subscribe olarak data içindeki counter değeri alınır ya da store select ile observable olarak da alınabilir. Bu durumda html içinde {{ (counter$ | async).counter }} ile gösterim sağlanır
-    this.store.select('counter').subscribe(data => {
-      this.counter = data.counter;
-      console.log("counter observable called");
+    // this.store.select('counter').subscribe(data => {
+    //   this.counter = data.counter;
+    //   console.log("counter observable called");
+    // })
+    this.store.select(getCounter).subscribe(counter => {
+      this.counter = counter;
     })
 
-    this.counter$ = this.store.select('counter');
+    // this.counter$ = this.store.select('counter');
+
+    this.counter$ = this.store.select(getCounter)
+
   }
 
 
--- ngrxEx/src/app/counter/custom-counter-input/custom-counter-input.component.ts
+++ ngrxEx/src/app/counter/custom-counter-input/custom-counter-input.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { changeChannelName, customIncrement } from '../state/counter.actions';
+import { getChannelName } from '../state/counter.selectors';
 import { CounterState } from '../state/counter.state';
 
 @Component({
@@ -12,16 +14,27 @@ export class CustomCounterInputComponent implements OnInit {
 
   value: number;
   channelName: string;
+  channelName$: Observable<string>
 
   constructor(private store: Store<{ counter: CounterState }>) { }
 
   ngOnInit(): void {
-    this.store.select('counter').subscribe(data => {
-      //burada data.counter yazmak istediğimde tip güvenli çalışma sayesinde hata verdi.
-      this.channelName = data.channelName;
-      //bu logun eklenmesinin sebebi selector kullanılmamasının dezavantajını görmektir
-      console.log("channel name observable called")
-    })
+    // this.store.select('counter').subscribe(data => {
+    //   //burada data.counter yazmak istediğimde tip güvenli çalışma sayesinde hata verdi.
+    //   this.channelName = data.channelName;
+    //   //bu logun eklenmesinin sebebi selector kullanılmamasının dezavantajını görmektir
+    //   console.log("channel name observable called")
+    // })
+
+    //artık yukarıdaki yapı selector yapısına uygun olarak yazılmakta
+
+    // this.store.select(getChannelName).subscribe(channelName => {
+    //   this.channelName = channelName;
+    // })
+
+    //subscribe olmaksızın object tipinde data alınır;
+    this.channelName$ = this.store.select(getChannelName);
+
   }
 
   onAdd() {
