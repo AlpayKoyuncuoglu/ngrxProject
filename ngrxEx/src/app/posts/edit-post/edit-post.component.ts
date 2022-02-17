25a885391ab8ad2ec7379eb8943ff6a74876c226
akoyuncuoglu <akoyuncuoglu@PC-KOYUNCUOGLU>
2022-02-17 09:56:52 +0300

    13th

--- ngrxEx/src/app/posts/edit-post/edit-post.component.ts
+++ ngrxEx/src/app/posts/edit-post/edit-post.component.ts
@@ -1,45 +1,75 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { updatePost } from './../state/posts.actions';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Post } from './../../models/posts.model';
+import { getPostById } from './../state/posts.selector';
+import { AppState } from './../../store/app.state';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Post } from 'src/app/models/posts.model';
-import { AppState } from 'src/app/store/app.state';
-import { getPostById } from '../state/posts.selector';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-edit-post',
   templateUrl: './edit-post.component.html',
-  styleUrls: ['./edit-post.component.css']
+  styleUrls: ['./edit-post.component.css'],
 })
-export class EditPostComponent implements OnInit {
-
+export class EditPostComponent implements OnInit, OnDestroy {
   post: Post;
   postForm: FormGroup;
-
-  constructor(private route: ActivatedRoute, private store: Store<AppState>) { }
+  postSubscription: Subscription;
+  constructor(
+    private route: ActivatedRoute,
+    private store: Store<AppState>,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       const id = params.get('id');
-      this.store.select(getPostById, { id }).subscribe(data => {
-        this.post = data;
-        this.createForm()
-      })
-      console.log(params);
-    })
+      this.postSubscription = this.store
+        .select(getPostById, { id })
+        .subscribe((data) => {
+          this.post = data;
+          this.createForm();
+        });
+    });
   }
 
   createForm() {
     this.postForm = new FormGroup({
       title: new FormControl(this.post.title, [
         Validators.required,
-        Validators.minLength(6)
+        Validators.minLength(6),
       ]),
       description: new FormControl(this.post.description, [
         Validators.required,
-        Validators.minLength(10)
-      ])
+        Validators.minLength(10),
+      ]),
     });
   }
 
+  onSubmit() {
+    if (!this.postForm.valid) {
+      return;
+    }
+
+    const title = this.postForm.value.title;
+    const description = this.postForm.value.description;
+
+    const post: Post = {
+      id: this.post.id,
+      title,
+      description,
+    };
+
+    //dispatch the action
+    this.store.dispatch(updatePost({ post }));
+    this.router.navigate(['posts']);
+  }
+
+  ngOnDestroy() {
+    if (this.postSubscription) {
+      this.postSubscription.unsubscribe();
+    }
+  }
 }
--- ngrxEx/src/app/posts/state/posts.reducer.ts
+++ ngrxEx/src/app/posts/state/posts.reducer.ts
@@ -1,22 +1,32 @@
-import { createReducer, on } from "@ngrx/store"
-import { Action } from "rxjs/internal/scheduler/Action";
-import { addPost } from "./posts.actions";
-import { initialState } from "./posts.state";
+import { addPost, updatePost } from './posts.actions';
+import { createReducer, on } from '@ngrx/store';
+import { initialState } from './posts.state';
 
-const _postReducer = createReducer(
+const _postsReducer = createReducer(
   initialState,
   on(addPost, (state, action) => {
-
     let post = { ...action.post };
 
     post.id = (state.posts.length + 1).toString();
+
     return {
       ...state,
-      posts: [...state.posts, post]
-    }
+      posts: [...state.posts, post],
+    };
+  }),
+  on(updatePost, (state, action) => {
+
+    const updatedPosts = state.posts.map((post) => {
+      return action.post.id === post.id ? action.post : post;
+    });
+
+    return {
+      ...state,
+      posts: updatedPosts,
+    };
   })
-)
+);
 
 export function postsReducer(state, action) {
-  return _postReducer(state, action);
+  return _postsReducer(state, action);
 }
