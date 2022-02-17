f2bb7cea67f22cab7bb280e6b0112d7ffb9cba02
akoyuncuoglu <akoyuncuoglu@PC-KOYUNCUOGLU>
2022-02-17 15:22:46 +0300

    17th

---
+++ ngrxEx/src/app/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  {
+    path: '', children: [
+      { path: '', redirectTo: 'login' },
+      { path: 'login', component: LoginComponent }
+    ]
+  }
+]
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class AuthModule { }
